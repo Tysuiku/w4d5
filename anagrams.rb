3040de52070c93b1c8c782bc30375277a9ed886a
Tysuiku <Timothydong41@gmail.com>
2023-02-03 15:16:05 -0500

    start anagrams

---
+++ anagrams.rb
@@ -0,0 +1,15 @@
+# def first_anagram?(str1, str2)
+#   final_arr = []
+#   str1.each_char.with_index do |char, i|
+#     str1.each_char.with_index do |char, j|
+#       if i < j
+#         final_arr << str1[i..j]
+#       end
+#     end
+#   end
+
+#   final_arr.select { |x| x == x.reverse }
+# end
+
+def first_anagram?(str1, str2)
+end
