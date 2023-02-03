6a2c59f9abb84c667321ce2d79822bbcc2d20ffb
Tysuiku <Timothydong41@gmail.com>
2023-02-03 14:56:58 -0500

    work on largest_countiguous_subsum_2

--- execution_time_diff.rb
+++ execution_time_diff.rb
@@ -1,3 +1,4 @@
+# O(n^2)
 def my_min(arr)
   num = 0
   i = 0
@@ -11,6 +12,7 @@ end
 # list = [0, 3, 5, 4, -5, 10, 1, 90]
 # p my_min(list)
 
+# O(n)
 def my_min_v2(arr)
   min_num = arr[0]
   arr.each { |ele| min_num = ele if min_num > ele }
@@ -19,3 +21,33 @@ end
 
 # list = [0, 3, 5, 4, -5, 10, 1, 90]
 # p my_min_v2(list)
+
+# O(n^2)
+def largest_contiguous_subsum(arr)
+  f_arr = []
+  arr.each_with_index do |ele, i|
+    arr.each_with_index do |ele, j|
+      if i <= j
+        f_arr << arr[i..j]
+      end
+    end
+  end
+  max = f_arr[0].sum
+  f_arr.each do |sub_arr|
+    max = sub_arr.sum if max < sub_arr.sum
+  end
+
+  max
+end
+
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list)
+
+def largest_contiguous_subsum_2(arr)
+
+  # largest_sum =
+  # current_sum =
+end
+
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list)
